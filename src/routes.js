dbcddf646ab6d7720ee616d7da944a840e5e4f5a
Tiago Pereira Dantas <devdantas@pop-os.localdomain>
2022-05-12 00:38:56 -0300

    delete api client

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
 const toSendEmailController = require("../src/Controllers/toSendEmailController");
-const SendEmailClientController = require("../src/Controllers/SendEmailClientController");
 
 const router = express.Router();
 
 router.post("/", toSendEmailController.CreateRegister);
-router.post("/client", SendEmailClientController.ToSendClientConfirm);
 
 module.exports = router
